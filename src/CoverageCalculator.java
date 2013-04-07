7405f06b0b9836567c8564e866ae82c6b4f82dc6
Mark Rutledge <rutledgemarkr@gmail.com>
2013-04-07 19:51:00 -0400

    Signed-off-by: Mark Rutledge <rutledgemarkr@gmail.com>

--- src/CoverageCalculator.java
+++ src/CoverageCalculator.java
@@ -7,7 +7,7 @@ public class CoverageCalculator {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showInternalMessageDialog(null, "Top", "THIS IS NEAT", JOptionPane.WARNING_MESSAGE);
+		JOptionPane.showMessageDialog(null, "Top", "THIS IS NEAT", JOptionPane.WARNING_MESSAGE);
 
 	}
 
