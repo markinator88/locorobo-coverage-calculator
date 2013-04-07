d5aed565e85aaf937df5075f53167c73f4c826b3
Mark Rutledge <rutledgemarkr@gmail.com>
2013-04-07 17:56:33 -0400

    Deleted test files
    Signed-off-by: Mark Rutledge <rutledgemarkr@gmail.com>

--- Coverage Calculator/src/Test.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-
-public class Test {
-	public static void main(String args[]) {
-		JOptionPane.showMessageDialog(null, "THIS IS NEAT!...if it works.", "Yo", JOptionPane.QUESTION_MESSAGE);
-	}
-}
