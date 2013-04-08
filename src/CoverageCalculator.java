795bb2bd031fd9a7eedfe62fe6946b91c7843d19
Doug Anderson <masterscat@gmail.com>
2013-04-07 22:49:58 -0400

    just a test.

--- src/CoverageCalculator.java
+++ src/CoverageCalculator.java
@@ -8,7 +8,7 @@ public class CoverageCalculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null, "Top", "THIS IS NEAT", JOptionPane.WARNING_MESSAGE);
-		// Added by Doug
+		// Added by Doug <
 		// Added by Paul
 		// Test Conflict 1
 		// Test Conflict 2
