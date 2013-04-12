8183a9ad178fc7bb8eff79f887d64a5138afe088
markinator88 <rutledgemarkr@gmail.com>
2013-04-12 19:49:26 -0400

    Signed-off-by: markinator88 <rutledgemarkr@gmail.com>

--- src/Project.java
+++ src/Project.java
@@ -1,10 +1,14 @@
 import java.util.*;
 
 public class Project {
-	private LinkedList<Room> roomList = new LinkedList();
+	private ArrayList<Room> roomList = new ArrayList<Room>();
+	private String projectName = new String();
 	
 	public void addRoom(Room r) {
 		roomList.add(r);
+	}
+	public void replaceRoom(int index, Room modifiedRoom) {
+		roomList.set(index, modifiedRoom);
 	}
 	public void removeRoom(int index){
 		roomList.remove(index);
