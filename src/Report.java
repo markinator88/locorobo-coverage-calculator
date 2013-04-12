3435e9482c4b41e3f155a0fa51a2967ae7fccec0
Doug Anderson <masterscat@gmail.com>
2013-04-11 22:22:24 -0400

    added

---
+++ src/Report.java
@@ -0,0 +1,17 @@
+
+public class Report {
+	/* From breeze notes.
+	 * PorjectManager {
+
+	    Report(project);
+
+	}
+
+	Report {
+	    Report(Project p) {
+	        //calculatations
+	        ReportView(this)
+	     }
+	}
+	 */
+}
