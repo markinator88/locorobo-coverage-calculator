89ce2dc1777c03c0d0d0b28c678cdd0b48439832
criquelmy <criquelmy@gmail.com>
2013-04-16 19:31:22 -0400

    ideas for materials list

---
+++ src/MaterialsList.java
@@ -0,0 +1,15 @@
+
+public class MaterialsList {
+	
+	// my understanding is that this class will use the objectes created in the material class
+	
+	// contains a list of all the materials that could be used for construction
+	// stores each object into an array of materials
+	// get procedure would take in the material name and return the cost per pallet after searching
+	// the array for the material name
+	// if no material found, return "material unavailable"
+	// get all procedure to display list of all avaliable materials
+	
+	
+
+}
