a620558fc1e768b13a2da15da0201941bdd437cc
Jeremy Vance <jvance@wcs.k12.va.us>
2013-04-17 18:48:15 -0400

    Room Class... created the array stuff...

--- src/Room.java
+++ src/Room.java
@@ -4,6 +4,26 @@ public class Room {
 	private String roomName = new String("new room");
 	private ArrayList surfaceList = new ArrayList();
 	
-	
-	
+	public void addSurface(Surface s) {
+		surfaceList.add(s);
+	}
+	public void replaceSurface(int index, Surface modifiedSurface){
+		surfaceList.set(index, modifiedSurface);
+			
+	}
+	public void removeSurface(int index){
+		surfaceList.remove(index);
+	}
+	public Surface getSurfaceAtIndex(int index){
+		return (Surface) surfaceList.get(index);
+		
+	}
+	public void setName(String n) {
+		roomName = n;
+		
+	}
+	public String getName(){
+		return roomName;
+	}
+
 }
