317a2514f11519e8cfacd6fcd514bc6c6843e615
p-mack <pmcannally@radford.edu>
2013-04-18 16:04:24 -0400

    Add JComboBox(s) and JTextArea(s) in SurfaceManager

--- src/SurfaceManager.java
+++ src/SurfaceManager.java
@@ -6,11 +6,23 @@ import java.awt.event.*;
 public class SurfaceManager extends JDialog {
 	
 	private JDialog thisDialog = this;
-	private JTextField heightTextField;
 	private JTextField lengthTextField;
 	private JTextField widthTextField;
 	private Surface startingSurface;
 	private Surface workingSurface;
+	private JTextField txtLength;
+	private JComboBox comboBox;
+	private JTextField txtWidth;
+	private JTextField txtHeight;
+	private JTextField textField_3;
+	private JLabel lblChooseMaterial;
+	private JLabel lblChooseCutoutType;
+	private JComboBox comboBox_1;
+	private JLabel lblSurfaceSize;
+	private JLabel lblCutoutSize;
+	private JButton btnSaveSurface;
+	private JLabel lblSurfaceList;
+	private JList list;
 	
 	public SurfaceManager(Dialog owner, Surface s) {
 		super(owner, true);
@@ -21,25 +33,13 @@ public class SurfaceManager extends JDialog {
 		setTitle("Surface Manager");
 		setBounds(200, 200, 700, 400);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 43, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.gridheight = 7;
-		gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 0;
-		gbc_scrollPane.gridy = 0;
-		getContentPane().add(scrollPane, gbc_scrollPane);
-		
 		String[] data = {"Trim", "Paint", "Floor Tile"};
-		JList surfaceListBox = new JList();
-		surfaceListBox.setBackground(Color.LIGHT_GRAY);
-		scrollPane.setViewportView(surfaceListBox);
 		
 		
 		widthTextField = new JTextField();
@@ -49,7 +49,7 @@ public class SurfaceManager extends JDialog {
 		gbc_widthTextField.anchor = GridBagConstraints.CENTER;
 		gbc_widthTextField.insets = new Insets(0, 110, 77,110);
 		gbc_widthTextField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_widthTextField.gridx = 2;
+		gbc_widthTextField.gridx = 3;
 		gbc_widthTextField.gridy = 0;
 		getContentPane().add(widthTextField, gbc_widthTextField);
 		widthTextField.setColumns(10);
@@ -60,112 +60,149 @@ public class SurfaceManager extends JDialog {
 		lengthTextField.addActionListener(new lengthTextFieldListener());
 		gbc_lengthTextField.anchor = GridBagConstraints.NORTHWEST;
 		gbc_lengthTextField.insets = new Insets(0, 110, 77,110);
-		gbc_lengthTextField.gridx = 2;
+		gbc_lengthTextField.gridx = 3;
 		gbc_lengthTextField.gridy = 0;
 		getContentPane().add(lengthTextField, gbc_lengthTextField);
 		lengthTextField.setColumns(10);
-		
-		
-		heightTextField = new JTextField();
-		heightTextField.setText("Height"); 
-		GridBagConstraints gbc_heightTextField = new GridBagConstraints();
-		heightTextField.addActionListener(new heightTextFieldListener());
-		gbc_heightTextField.anchor = GridBagConstraints.SOUTH;
-		gbc_heightTextField.insets = new Insets(0, 110, 77,110);
-		gbc_heightTextField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_heightTextField.gridx = 2;
-		gbc_heightTextField.gridy = 0;
-		getContentPane().add(heightTextField, gbc_heightTextField);
 		lengthTextField.setColumns(10);
-		//Button to add trim
-			
-		JButton newTrimButton = new JButton("Add Trim");
-		newTrimButton.addActionListener(new NewTrimListener());
-		GridBagConstraints gbc_newTrimButton = new GridBagConstraints();
-		gbc_newTrimButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_newTrimButton.insets = new Insets(0, 0, 5, 0);
-		gbc_newTrimButton.gridx = 1;
-		gbc_newTrimButton.gridy = 1;
-		getContentPane().add(newTrimButton, gbc_newTrimButton);
 		
-		/* JButton newSurfaceButton = new JButton("Add Tile Flooring");
-		newSurfaceButton.addActionListener(new NewTileListener());
-		GridBagConstraints gbc_TileButton = new GridBagConstraints();
-		gbc_newSurfaceButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_newSurfaceButton.insets = new Insets(0, 0, 5, 0);
-		gbc_newSurfaceButton.gridx = 1;
-		gbc_newSurfaceButton.gridy = 1;
-		getContentPane().add(newTileButton, gbc_newTileButton); */
+		lblChooseMaterial = new JLabel("Choose Material");
+		GridBagConstraints gbc_lblChooseMaterial = new GridBagConstraints();
+		gbc_lblChooseMaterial.insets = new Insets(0, 0, 5, 5);
+		gbc_lblChooseMaterial.gridx = 0;
+		gbc_lblChooseMaterial.gridy = 1;
+		getContentPane().add(lblChooseMaterial, gbc_lblChooseMaterial);
 		
-		/*
-		JButton modifySurfaceButton = new JButton("Add Paint");
-		modifySurfaceButton.addActionListener(new ModifySurfaceListener());
-		GridBagConstraints gbc_modifySurfaceButton = new GridBagConstraints();
-		gbc_modifySurfaceButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_modifySurfaceButton.insets = new Insets(0, 0, 5, 0);
-		gbc_modifySurfaceButton.gridx = 1;
-		gbc_modifySurfaceButton.gridy = 2;
-		getContentPane().add(modifySurfaceButton, gbc_modifySurfaceButton);
-		*/
-		/*
-		JButton deleteSurfaceButton = new JButton("Delete Surface");
-		deleteSurfaceButton.addActionListener(new DeleteSurfaceListener());
-		GridBagConstraints gbc_deleteSurfaceButton = new GridBagConstraints();
-		gbc_deleteSurfaceButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_deleteSurfaceButton.insets = new Insets(0, 0, 5, 0);
-		gbc_deleteSurfaceButton.gridx = 1;
-		gbc_deleteSurfaceButton.gridy = 3;
-		getContentPane().add(deleteSurfaceButton, gbc_deleteSurfaceButton);
-		*/
+		lblSurfaceSize = new JLabel("Surface Size");
+		GridBagConstraints gbc_lblSurfaceSize = new GridBagConstraints();
+		gbc_lblSurfaceSize.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSurfaceSize.gridx = 1;
+		gbc_lblSurfaceSize.gridy = 1;
+		getContentPane().add(lblSurfaceSize, gbc_lblSurfaceSize);
+		
+		comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 0;
+		gbc_comboBox.gridy = 2;
+		getContentPane().add(comboBox, gbc_comboBox);
+		
+		txtLength = new JTextField();
+		txtLength.setText("Height/Legth");
+		GridBagConstraints gbc_txtLength = new GridBagConstraints();
+		gbc_txtLength.insets = new Insets(0, 0, 5, 5);
+		gbc_txtLength.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtLength.gridx = 1;
+		gbc_txtLength.gridy = 2;
+		getContentPane().add(txtLength, gbc_txtLength);
+		
+		txtWidth = new JTextField();
+		txtWidth.setText("Width");
+		GridBagConstraints gbc_txtWidth = new GridBagConstraints();
+		gbc_txtWidth.insets = new Insets(0, 0, 5, 5);
+		gbc_txtWidth.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtWidth.gridx = 2;
+		gbc_txtWidth.gridy = 2;
+		getContentPane().add(txtWidth, gbc_txtWidth);
+		
+		lblChooseCutoutType = new JLabel("Choose Cutout Type");
+		GridBagConstraints gbc_lblChooseCutoutType = new GridBagConstraints();
+		gbc_lblChooseCutoutType.anchor = GridBagConstraints.SOUTH;
+		gbc_lblChooseCutoutType.insets = new Insets(0, 0, 5, 5);
+		gbc_lblChooseCutoutType.gridx = 0;
+		gbc_lblChooseCutoutType.gridy = 3;
+		getContentPane().add(lblChooseCutoutType, gbc_lblChooseCutoutType);
+		
+		lblCutoutSize = new JLabel("Cutout Size");
+		GridBagConstraints gbc_lblCutoutSize = new GridBagConstraints();
+		gbc_lblCutoutSize.anchor = GridBagConstraints.SOUTH;
+		gbc_lblCutoutSize.insets = new Insets(0, 0, 5, 5);
+		gbc_lblCutoutSize.gridx = 1;
+		gbc_lblCutoutSize.gridy = 3;
+		getContentPane().add(lblCutoutSize, gbc_lblCutoutSize);
+		
+		comboBox_1 = new JComboBox();
+		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
+		gbc_comboBox_1.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox_1.gridx = 0;
+		gbc_comboBox_1.gridy = 4;
+		getContentPane().add(comboBox_1, gbc_comboBox_1);
+		
+		txtHeight = new JTextField();
+		txtHeight.setText("Height");
+		txtHeight.setColumns(10);
+		GridBagConstraints gbc_txtHeight = new GridBagConstraints();
+		gbc_txtHeight.insets = new Insets(0, 0, 5, 5);
+		gbc_txtHeight.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtHeight.gridx = 1;
+		gbc_txtHeight.gridy = 4;
+		getContentPane().add(txtHeight, gbc_txtHeight);
+		
+		textField_3 = new JTextField();
+		textField_3.setText("Width");
+		textField_3.setColumns(10);
+		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_3.gridx = 2;
+		gbc_textField_3.gridy = 4;
+		getContentPane().add(textField_3, gbc_textField_3);
+		
+		btnSaveSurface = new JButton("Save Cutout");
+		GridBagConstraints gbc_btnSaveSurface = new GridBagConstraints();
+		gbc_btnSaveSurface.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSaveSurface.gridx = 0;
+		gbc_btnSaveSurface.gridy = 5;
+		getContentPane().add(btnSaveSurface, gbc_btnSaveSurface);
+		
+		lblSurfaceList = new JLabel("Cutout List");
+		GridBagConstraints gbc_lblSurfaceList = new GridBagConstraints();
+		gbc_lblSurfaceList.anchor = GridBagConstraints.EAST;
+		gbc_lblSurfaceList.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSurfaceList.gridx = 1;
+		gbc_lblSurfaceList.gridy = 5;
+		getContentPane().add(lblSurfaceList, gbc_lblSurfaceList);
+		
+		list = new JList();
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.insets = new Insets(0, 0, 5, 5);
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 2;
+		gbc_list.gridy = 5;
+		getContentPane().add(list, gbc_list);
 		Component verticalStrut = Box.createVerticalStrut(20);
 		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
-		gbc_verticalStrut.insets = new Insets(0, 0, 5, 0);
+		gbc_verticalStrut.insets = new Insets(0, 0, 5, 5);
 		gbc_verticalStrut.gridx = 1;
-		gbc_verticalStrut.gridy = 4;
+		gbc_verticalStrut.gridy = 8;
 		getContentPane().add(verticalStrut, gbc_verticalStrut);
 		
 		JButton saveExitButton = new JButton("Save and Exit");
 		saveExitButton.addActionListener(new SaveExitListener());
 		GridBagConstraints gbc_saveExitButton = new GridBagConstraints();
 		gbc_saveExitButton.fill = GridBagConstraints.HORIZONTAL;
-		gbc_saveExitButton.insets = new Insets(0, 0, 5, 0);
+		gbc_saveExitButton.insets = new Insets(0, 0, 5, 5);
 		gbc_saveExitButton.gridx = 1;
-		gbc_saveExitButton.gridy = 5;
+		gbc_saveExitButton.gridy = 9;
 		getContentPane().add(saveExitButton, gbc_saveExitButton);
 		
 		JButton exitNoSaveButton = new JButton("Exit Without Saving");
 		exitNoSaveButton.addActionListener(new ExitNoSaveListener());
 		GridBagConstraints gbc_exitNoSaveButton = new GridBagConstraints();
+		gbc_exitNoSaveButton.insets = new Insets(0, 0, 0, 5);
 		gbc_exitNoSaveButton.fill = GridBagConstraints.HORIZONTAL;
 		gbc_exitNoSaveButton.anchor = GridBagConstraints.NORTH;
 		gbc_exitNoSaveButton.gridx = 1;
-		gbc_exitNoSaveButton.gridy = 6;
+		gbc_exitNoSaveButton.gridy = 10;
 		getContentPane().add(exitNoSaveButton, gbc_exitNoSaveButton);
-	}
-
-	private class NewTrimListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-            JTextArea textArea = new JTextArea();
-            textArea.setEditable(true);
-            JScrollPane scrollPane = new JScrollPane(textArea);
-            scrollPane.requestFocus();
-            textArea.requestFocusInWindow();
-            scrollPane.setPreferredSize(new Dimension(800, 600));
-            JOptionPane.showMessageDialog(scrollPane,
-                    "Paste Info");
-            String info = textArea.getText();		
-		}
 	}
 	public Surface showDialog() {
 		// TODO finish logic for surface
 		this.setVisible(true);
 		return startingSurface;
 	}
-	private class heightTextFieldListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			System.out.println("Text= " + heightTextField.getText());
-		      }
-			}
 	private class widthTextFieldListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Text= " + widthTextField.getText());
