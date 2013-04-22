d757c72a837de2b0673566386ac1bd006de723c5
markinator88 <rutledgemarkr@gmail.com>
2013-04-22 07:04:09 -0400

    Signed-off-by: markinator88 <rutledgemarkr@gmail.com>

--- src/Material.java
+++ src/Material.java
@@ -5,7 +5,8 @@ public class Material {
 * Should we have one Material class or should we create multiple classes
 * (that may inherit from the Material class) for different types of materials?
 */
-
+	private int UID;
+	private String materialName;
 	private int materialType;
 	private SKUList skuList;
 	
--- src/TabSeparatedFileReader.java
+++ src/TabSeparatedFileReader.java
@@ -4,15 +4,15 @@ import java.io.*;
 public class TabSeparatedFileReader {
  
     public static MaterialsList readFiles() throws Exception {
-        /**
+        /*
          * Source files to read data from.
          */
-
+    	MaterialsList materialsList = new MaterialsList();
+    	
         String materialsFileName = "materials.tsv";
         String skuFileName = "sku.tsv"; // SKU = stock-keeping unit
  
-     	MaterialsList materialsList = new MaterialsList();
-        /**
+        /*
          * Creating a buffered reader to read the file
          */
 
@@ -20,22 +20,23 @@ public class TabSeparatedFileReader {
                 new FileReader(materialsFileName));
  
         String line;
- //herp
- //derp
-        /**
+
+        /*
          * Looping the read block until all lines in the file are read.
          */
         while ((line = bReader.readLine()) != null) {
  
-            /**
+            /*
              * Splitting the content of tabbed separated line
              */
             String dataValue[] = line.split("\t");
             int uniqueID = Integer.parseInt(dataValue[0]);
             String materialName = dataValue[1];
             int materialType = Integer.parseInt(dataValue[2]);
+            
+            // materialsList.
  
-            /**
+            /*
              * Printing the value read from file to the console
              */
             //System.out.println(value1 + "\t" + value2 + "\t" + value3 + "\t"
