78e01f1fffaba48b648a5620b2504792956c9684
Doug Anderson <masterscat@gmail.com>
2013-04-25 22:41:07 -0400

    Added the GUI

--- src/Report.java
+++ src/Report.java
@@ -1,8 +1,91 @@
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
-public class Report {
-	    Report(Project p) {
-	        //calculatations
-	        //ReportView(this)
-	     				  }
-					} 
 
+public class Report extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Report() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{153, 143, 0};
+		gridBagLayout.rowHeights = new int[]{15, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel label = new JLabel("Coverage Calculator");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.insets = new Insets(0, 0, 5, 0);
+		gbc_label.anchor = GridBagConstraints.NORTHWEST;
+		gbc_label.gridx = 1;
+		gbc_label.gridy = 0;
+		add(label, gbc_label);
+		
+		JLabel lblNewLabel = new JLabel("Paint Needed");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 2;
+		add(lblNewLabel, gbc_lblNewLabel);
+		
+		JTextArea textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 1;
+		gbc_textArea.gridy = 2;
+		add(textArea, gbc_textArea);
+		
+		JLabel lblNewLabel_1 = new JLabel("Trim Needed");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 4;
+		add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JTextArea textArea_1 = new JTextArea();
+		GridBagConstraints gbc_textArea_1 = new GridBagConstraints();
+		gbc_textArea_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea_1.fill = GridBagConstraints.BOTH;
+		gbc_textArea_1.gridx = 1;
+		gbc_textArea_1.gridy = 4;
+		add(textArea_1, gbc_textArea_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Tiles Needed");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 6;
+		add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JTextArea textArea_2 = new JTextArea();
+		GridBagConstraints gbc_textArea_2 = new GridBagConstraints();
+		gbc_textArea_2.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea_2.fill = GridBagConstraints.BOTH;
+		gbc_textArea_2.gridx = 1;
+		gbc_textArea_2.gridy = 6;
+		add(textArea_2, gbc_textArea_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Subtract Cutout");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 8;
+		add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		JTextArea textArea_3 = new JTextArea();
+		GridBagConstraints gbc_textArea_3 = new GridBagConstraints();
+		gbc_textArea_3.fill = GridBagConstraints.BOTH;
+		gbc_textArea_3.gridx = 1;
+		gbc_textArea_3.gridy = 8;
+		add(textArea_3, gbc_textArea_3);
+
+	}
+
+}
