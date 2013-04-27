a4941cb133e3d80699da9e8126b9560ae782c182
markinator88 <rutledgemarkr@gmail.com>
2013-04-27 18:59:31 -0400

    Added more Javadoc comments.
    Signed-off-by: markinator88 <rutledgemarkr@gmail.com>

--- src/SKU.java
+++ src/SKU.java
@@ -1,10 +1,19 @@
-
+/**
+ * used to store and modify information about SKUs (stock-keeping units)
+ */
 public class SKU {
 	private int foreignKey;
 	private String skuName;
 	private double baseUnits;
 	private double price;
 	
+	/**
+	 * creates a SKU with the specified parameters
+	 * @param foreignKey foreign key to link with a material
+	 * @param skuName name of SKU
+	 * @param baseUnits amount of base/standard units in SKU
+	 * @param price of the SKU
+	 */
 	public SKU(int foreignKey, String skuName, double baseUnits, double price) {
 		this.foreignKey = foreignKey;
 		this.skuName = skuName;
@@ -12,18 +21,34 @@ public class SKU {
 		this.price = price;
 	}
 	
+	/**
+	 * returns the foreign key for the SKU to link to material
+	 * @return foreign key
+	 */
 	public int getForeignKey() {
 		return foreignKey;
 	}
 	
+	/**
+	 * returns the name of the SKU
+	 * @return
+	 */
 	public String getSKUName() {
 		return skuName;
 	}
 	
+	/**
+	 * returns the number of base/standard units of material in this SKU
+	 * @return number of base/standard units of material in this SKU
+	 */
 	public double getBaseUnits() {
 		return baseUnits;
 	}
 	
+	/**
+	 * returns the price of one SKU
+	 * @return price
+	 */
 	public double getPrice() {
 		return price;
 	}
--- src/Surface.java
+++ src/Surface.java
@@ -1,12 +1,10 @@
 import java.util.ArrayList;
 
-
+/**
+ * used to store and modify a description of the room
+ */
 public class Surface {
 	
-	/**
-	 * Surface class 
-	 *  
-	 */
 	private String surfaceName;
     private String surfaceType; 
 	private double surfaceXdim; 
@@ -17,12 +15,20 @@ public class Surface {
 	
 	private ArrayList<Cutout> cutoutList = new ArrayList<Cutout>();
 	
+	/**
+	 * creates a new surface with no information
+	 */
 	Surface (){
 		
 		surfaceName = new String("new surface");
 	}
 	
-	//
+	/**
+	 * creates a new surface with specified dimensions 
+	 * @param surfaceXdim
+	 * @param surfaceYdim
+	 * @param surfaceType
+	 */
 	Surface(double surfaceXdim, double surfaceYdim, String surfaceType) {
 		//
 		surfaceName = new String("new surface");
@@ -30,80 +36,164 @@ public class Surface {
 		this.surfaceYdim = surfaceYdim;
 		this.surfaceType = surfaceType; 
 		}
-	
-	Surface(Surface r) { // copy constructor
-		this.surfaceName = new String(r.getName());
+	/**
+	 * creates a copy of the specified surface
+	 * @param surface to be copied
+	 */
+	Surface(Surface s) { // copy constructor
+		this.surfaceName = new String(s.getName());
 		this.cutoutList = new ArrayList<Cutout>();
 		for (Cutout cutout : cutoutList) {
 			this.cutoutList.add(new Cutout(cutout));
 		}
 	}
 	
-	public void addCutout(Cutout s) {
-		cutoutList.add(s); 
+	/**
+	 * adds a cutout to the surface
+	 * @param cutout to be added
+	 */
+	public void addCutout(Cutout c) {
+		cutoutList.add(c); 
 	}
+	
+	/**
+	 * replaces a cutout
+	 * @param index of cutout to be replace
+	 * @param modifiedCutout replacement cutout
+	 */
 	public void replaceCutout(int index, Cutout modifiedCutout) {
 		cutoutList.set(index, modifiedCutout);
 	}
 	
-	
-	
-	public void removeSurface(int index) {
+	/**
+	 * removes a cutout
+	 * @param index of cutout to be removed
+	 */
+	public void removeCutout(int index) {
 		cutoutList.remove(index);
 	}
 
+	/**
+	 * returns a cutout
+	 * @param index of cutout
+	 * @return cutout
+	 */
 	public Cutout getCutoutAtIndex(int index) {
 		return cutoutList.get(index);
 	}
 	
+	/**
+	 * returns number of cutouts
+	 * @return number of cutouts
+	 */
 	public int getListSize() {
 		return cutoutList.size();
 	}
 	
+	/**
+	 * sets name of surface
+	 * @param surface name
+	 */
 	public void setName(String s) {
 		surfaceName = s;
 	}
 	
+	/**
+	 * gets the name of the surface
+	 * @return surface name
+	 */
 	public String getSurfaceName() {
 		return surfaceName;
 	}
+	
+	/**
+	 * sets the name of the surface
+	 * @param surfaceName
+	 */
 	public void setSurfaceName(String surfaceName) {
 		this.surfaceName = surfaceName;
 	}
+	
+	/**
+	 * returns the type of surface
+	 * @return type of surface
+	 */
 	public String getSurfaceType() {
 		return surfaceType;
 	}
-	public void setSurfaceType(String surfaceType2) {
-		this.surfaceType = surfaceType2;
+	
+	/**
+	 * sets the type of surface
+	 * @param surfaceType
+	 */
+	public void setSurfaceType(String surfaceType) {
+		this.surfaceType = surfaceType;
 	}
+	
+	/**
+	 * gets the X dimension
+	 * @return x dimension
+	 */
 	public double getSurfaceXdim() {
 		return surfaceXdim;
 	}
+	
+	/**
+	 * sets the x dimension
+	 * @param surfaceXdim
+	 */
 	public void setSurfaceXdim(double surfaceXdim) {
 		this.surfaceXdim = surfaceXdim;
 	}
+	
+	/**
+	 * gets the surface y dimension
+	 * @return
+	 */
 	public double getSurfaceYdim() {
 		return surfaceYdim;
 	}
+	
+	/**
+	 * sets the surface y dimension
+	 * @param surfaceYdim
+	 */
 	public void setSurfaceYdim(double surfaceYdim) {
 		this.surfaceYdim = surfaceYdim;
 	}
+	
+	/**
+	 * sets the list of cutouts
+	 * @param cutoutList
+	 */
 	public void setCutoutList(ArrayList<Cutout> cutoutList) {
 		this.cutoutList = cutoutList;
 	}
+	
+	/**
+	 * returns the surface name
+	 * @return surface
+	 */
 	public String getName() {
 		return surfaceName;
 	}
 	
+	/**
+	 * returns the list of cutouts
+	 * @return list of cutouts
+	 */
 	public ArrayList<Cutout> getCutoutList() {
 		return (ArrayList<Cutout>)cutoutList.clone();
 	}
 	
+	/**
+	 * returns the names of all cutouts
+	 * @return names of all cutouts
+	 */
 	public String[] toStringArray() {
 		String returnArray[] = new String[cutoutList.size()];
 		for (int n=0; n<cutoutList.size(); n++) {
 			returnArray[n] = cutoutList.get(n).getName();
-			
 		}
 		return returnArray;
 	}
